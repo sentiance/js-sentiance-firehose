fd391d2f97af72f5f9d7e9a60465207c37a9eee3
Jiri Crispeyn <jiri.crispeyn@gmail.com>
2017-01-12 14:31:18 +0100

    Implement createSubscription in connect fn

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 var SentianceFirehose = (function () {
   var connect = function (appId, streamDefinitionId, bearerToken, config) {
-    console.log(appId);
-    console.log(streamDefinitionId);
-    console.log(bearerToken);
-    console.log(config);
-  }
+    createSubscription(appId, streamDefinitionId, bearerToken);
+  };
 
   var createSubscription = function (appId, streamDefinitionId, bearerToken) {
     var http = new XMLHttpRequest();
