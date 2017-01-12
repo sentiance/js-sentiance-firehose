d88c5d9bb1c0755b6dbe526c0ebb9a375d94a43c
Jiri Crispeyn <jiri.crispeyn@gmail.com>
2017-01-12 14:30:39 +0100

    Create createSubscription fn to get subscription id and token

--- src/index.js
+++ src/index.js
@@ -6,4 +6,39 @@ var SentianceFirehose = (function () {
     console.log(config);
   }
 
-};
+  var createSubscription = function (appId, streamDefinitionId, bearerToken) {
+    var http = new XMLHttpRequest();
+    var body = {
+      query: '\
+        mutation($app_id: String!, $stream_definition_id: String!) {\
+          createSubscription(app_id:$app_id, stream_definition_id: $stream_definition_id) {\
+            id\
+            token\
+          }\
+        }\
+      ',
+      variables: {
+        app_id: appId,
+        stream_definition_id: streamDefinitionId
+      }
+    }
+
+    http.open('POST', 'https://api.sentiance.com/v2/gql', true);
+    http.setRequestHeader('Content-Type', 'application/json');
+    http.setRequestHeader('Authorization', 'Bearer ' + bearerToken);
+    http.onreadystatechange = function () {
+      if (http.readyState === 4 && http.status === 200) {
+        var res = JSON.parse(http.responseText);
+        var id = res.data.createSubscription.id;
+        var token = res.data.createSubscription.token;
+
+        initFirehoseConnection(id, token);
+      }
+    }
+    http.send(JSON.stringify(body));
+  };
+
+  return {
+    connect: connect
+  };
+})();
